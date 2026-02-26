a9f0cc8074fda3bcfd584ab34b7307e1ace9eaba
bgovil <bgovil@ciena.com>
2026-02-26 17:31:04 +0530

    list devices component placed on the main app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DeviceList } from './components/device-list/device-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DeviceList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('assignment-frontend');
+  protected readonly title = signal('Device Inventory Management');
 }
