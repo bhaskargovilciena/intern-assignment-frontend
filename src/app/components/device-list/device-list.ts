96fc57d09f17e07cfc5a0e6614f9a0f9c8fd5c91
bgovil <bgovil@ciena.com>
2026-02-26 17:30:43 +0530

    device list component added

---
+++ src/app/components/device-list/device-list.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectorRef, Component, inject, NgZone, OnInit } from '@angular/core';
+import { DeviceService } from '../../services/device-service';
+import { Device } from '../../classes/device';
+import { ShelfPosition } from '../../classes/shelf-position';
+
+interface Result {
+  device:Device,
+  shelfPositions:ShelfPosition[]
+}
+
+@Component({
+  selector: 'app-device-list',
+  imports: [],
+  templateUrl: './device-list.html',
+  styleUrl: './device-list.css',
+})
+export class DeviceList implements OnInit {
+  devices:Result[] = []
+
+  constructor(private deviceService:DeviceService, private cdr:ChangeDetectorRef) {}
+  ngZone = inject(NgZone)
+
+  ngOnInit():void {
+    this.getAllDevices();
+  }
+
+  getAllDevices():void {
+    this.deviceService.getAllDevices().subscribe(result => {
+      console.log(result)
+      this.devices = [...result]
+      this.cdr.detectChanges()
+    })
+  }
+}
