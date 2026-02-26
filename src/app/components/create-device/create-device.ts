a1c60e3432d2ff770f4c9c6cefba1f14cd535075
bgovil <bgovil@ciena.com>
2026-02-26 17:45:25 +0530

    create device component intialised

---
+++ src/app/components/create-device/create-device.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-device',
+  imports: [],
+  templateUrl: './create-device.html',
+  styleUrl: './create-device.css',
+})
+export class CreateDevice {
+
+}
